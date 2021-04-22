4015e9b7a7cea144cc8691561cb0f205defba7f4
Jason Ostrom <jpo@pobox.com>
2021-04-22 08:09:53 -0700

    Changes to mkdocs

---
+++ aws/modules/ws2019-vm/output/bootstrap-rendered.ps1
@@ -0,0 +1,74 @@
+<powershell>
+
+# Beginning of bootstrap script
+# This script bootstraps the Windows system and is loaded through user data
+#
+
+# Create Terraform directory 
+$path = 'C:\terraform'
+if (-not (Test-Path $path)) {
+  New-Item $path -ItemType directory
+}
+
+# Set logfile and function for writing logfile
+$logfile = "C:\Terraform\user_data.log"
+Function lwrite {
+    Param ([string]$logstring)
+    Add-Content $logfile -value $logstring
+}
+
+$mtime = Get-Date
+lwrite("$mtime Starting bootstrap powershell script")
+
+$user = "RTCAdmin"
+$test = Get-LocalUser | Where-Object {$_.Name -eq $user}
+if ( -not $test) {
+  $mtime = Get-Date
+  lwrite("$mtime Creating new local username: $user")
+  & net user $user Password123 /add /y
+  lwrite("$mtime Adding username to local Administrators Group")
+  & net localgroup administrators $user /add
+}
+
+### Force Enabling WinRM and skip profile check
+$mtime = Get-Date
+lwrite("$mtime Enabling PSRemoting SkipNetworkProfileCheck")
+Enable-PSRemoting -SkipNetworkProfileCheck -Force 
+
+$mtime = Get-Date
+lwrite("$mtime Set Execution Policy Unrestricted")
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
+$ComputerName = "$env:computername"
+$RemoteHostName = $ComputerName + ".rtc.local" 
+$Cert = New-SelfSignedCertificate -DnsName $RemoteHostName, $ComputerName `
+    -CertStoreLocation "cert:\LocalMachine\My" `
+    -FriendlyName "Test WinRM Cert"
+
+$Cert | Out-String
+
+$Thumbprint = $Cert.Thumbprint
+
+lwrite("$mtime Enable HTTPS in WinRM")
+$WinRmHttps = "@{Hostname=`"$RemoteHostName`"; CertificateThumbprint=`"$Thumbprint`"}"
+winrm create winrm/config/Listener?Address=*+Transport=HTTPS $WinRmHttps
+
+lwrite("$mtime Set Basic Auth in WinRM")
+$WinRmBasic = "@{Basic=`"true`"}"
+winrm set winrm/config/service/Auth $WinRmBasic
+
+lwrite("$mtime Open Firewall Ports and disable firewall just in case")
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=5986
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+### Force Enabling WinRM and skip profile check
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+
+# Set Trusted Hosts * for WinRM HTTPS
+Set-Item -Force wsman:\localhost\client\trustedhosts *
+
+$mtime = Get-Date
+lwrite("$mtime End bootstrap powershell script")
+</powershell>
+<persist>true</persist>
--- docs/index.md
+++ docs/index.md
@@ -1,11 +1,9 @@
 # Overview
-Multi-use Hybrid + Identity Cyber Range implementing a small Active Directory Do
-main in Azure alongside Azure AD and Azure Domain Services.  Automated templates
- for building your own Pentest / Red Team / Cyber Range in the Azure cloud!  Pur
-ple Cloud is a small Active Directory enterprise deployment automated with Terra
-form / Ansible Playbook templates to be deployed in Azure.  Purple Cloud also in
-cludes an adversary node accessible over RDP as well as a SIEM, DFIR, & Live Response
- system (Velociraptor + HELK).
+Cyber Range deployment of HELK and Velociraptor!  Automated terraform deployment
+ of one system running HELK + Velociraptor server with one registered Windows endpoint
+in Azure or AWS.  A collection of Terraform and Ansible scripts that automatically
+(and quickly) deploys a small HELK + Velociraptor R&D lab.
+
 
 ![](images/bce.png)
 
